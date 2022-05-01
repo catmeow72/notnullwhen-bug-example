6160557686453b4c4525ab595b9cd71b42cef9cb
Cat Meowmeow <catmeowmeow36@gmail.com>
2022-05-01 12:36:35 -0700

    Simple example

--- Program.cs
+++ Program.cs
@@ -1,61 +1,16 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-#region Functions
-/// <Summary>
-/// Either returns true and sets the output to "Meow!", or returns false and sets the output to null.
-/// Note the NotNullWhen attribute. This should tell the compiler that the value is not null when the function returns <c>true</c>
-/// </Summary>
-/// <Param name="ExampleInt">If even, the output will not be null. Otherwise, it will be.</Param>
-/// <Param name="OutputString">The string to set.</Param>
-/// <Returns><c>true</c> if the value is not null, <c>false</c> otherwise.</Returns>
-bool Example_NullWhenTrue(int ExampleInt, [NotNullWhen(false)] out string? OutputString) {
-    if (ExampleInt % 2 == 0) {
-        OutputString = "Meow!";
-        return false;
-    } else {
-        OutputString = null;
-        return true;
-    }
+bool Example([NotNullWhen(true)] out string? output) {
+  output = "Hello, world!";
+  return true;
 }
 
-/// <Summary>
-/// A function to test the string (class) values with
-/// This function is used with the String class
-/// </Summary>
-/// <Param name="input">A non-null string to display</Param>
-/// <Exception cref="System.ArgumentNullException">Thrown when the string is null. Used for the examples that should throw exceptions when running the project.</Exception>
-void TestString([MaybeNull]string input) {
-    if (input == null) {
-        throw new ArgumentNullException(nameof(input));
-    }
-    Console.Out.WriteLine(input);
-}
-#endregion
-
-#region Tests with functions that return true when the output is null and return false when the output isn't null
-// The nullable strings to test with.
-string? NotNullTest;
-string? AlwaysNullTest;
-
-// Test the function call.
-bool IsNull_ShouldNotBeNull = Example_NullWhenTrue(2, out NotNullTest);
-bool IsNull_ShouldBeNull = Example_NullWhenTrue(1, out AlwaysNullTest);
-bool IsNull_ShouldNotBeNull_NewVar = Example_NullWhenTrue(2, out var OutputStringNewVar);
-
-// Check if each of the test values are null based on the function return value.
-// Then call a function that requires a non-null string, but only when the test values aren't null.
-
-// The below function call should always run because the value will never be null.
-if (!IsNull_ShouldNotBeNull) {
-    TestString(NotNullTest);
+void RequiresNonNull(string input) {
+	Console.WriteLine(input);
 }
 
-// The below function call should never run as the value will always be null
-if (!IsNull_ShouldBeNull) {
-    TestString(AlwaysNullTest);
-}
+bool isNotNull = Example(out var output);
 
-if (!IsNull_ShouldNotBeNull_NewVar) {
-    TestString(OutputStringNewVar);
+if (isNotNull) {
+	RequiresNonNull(output);
 }
-#endregion
